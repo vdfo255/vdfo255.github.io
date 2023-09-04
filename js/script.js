97f912f607ae4203e2f98f24a87c32394e44791a
vdfo255 <143737248+vdfo255@users.noreply.github.com>
2023-09-04 19:55:30 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
-  console.log('loaded');
     
-  //! ========== ASIDE CLOSE/OPEN ==========
+  //! ============= ASIDE CLOSE/OPEN =============
   const asideBar = document.querySelector('.aside'),
     asideCloseBtn = asideBar.querySelector('.aside__swipe'),
     asideCloseBtnImg = asideCloseBtn.querySelector('.aside__swipe-img'),
@@ -20,50 +19,100 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   })
 
-  //! ========== WEATHER FETCHING & DISPLAYING ==========
+
+  //! ============= WEATHER FETCHING & DISPLAYING =============
   const WEATHER_APIKEY = 'e732a92c66574d856b927e390c09674e',
     searchBar = document.querySelector('.weather__search-input'),
-    searchBtn = document.querySelector('.weather__search-btn'),    
-    weatherCard = document.querySelector('.weather__display'),
-    currWeatherIco = document.querySelector('#currIco'),
-    currWeahterTemp = document.querySelector('#currTemp'),
-    currWeahterDescr = document.querySelector('#currDescr'),
-    currWeatherCity = document.querySelector('#currCity'),
-    currWeatherHumidity = document.querySelector('#currHumidity'),
-    currWeatherWind = document.querySelector('#currWind');
+    searchBtn = document.querySelector('.weather__search-btn'),  
+    currWeatherBlock = document.querySelector('.weather__current'),  
+    currWeatherIco = currWeatherBlock.querySelector('#currIco'),
+    currWeahterTemp = currWeatherBlock.querySelector('#currTemp'),
+    currWeahterDescr = currWeatherBlock.querySelector('#currDescr'),
+    currWeatherCity = currWeatherBlock.querySelector('#currCity'),
+    currWeatherDate = currWeatherBlock.querySelector('#currDate'),
+    currWeatherSunrise = currWeatherBlock.querySelector('#currSunrise'),
+    currWeatherSunset = currWeatherBlock.querySelector('#currSunset'),
+    currWeatherHumidity = currWeatherBlock.querySelector('#currHumidity'),
+    currWeatherWind = currWeatherBlock.querySelector('#currWind'),
+    currWeatherVisibility = currWeatherBlock.querySelector('#currVisibility'),
+    currWeatherPressure = currWeatherBlock.querySelector('#currPressure');
 
-  //* ========== fetching weather ==========
-  function searchWeather(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${WEATHER_APIKEY}`)
+  //* ========== formating city value before fetching CURRENT weather ==========
+  function search() {
+    let formatedCity = searchBar.value.trim();
+    fetchCurrWeather(formatedCity);
+  }
+
+  //* ========== fetching CURRENT weather ==========
+  function fetchCurrWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${WEATHER_APIKEY}`) //? https://api.openweathermap.org/data/2.5/weather?q=poznan&units=metric&appid=e732a92c66574d856b927e390c09674e
     .then((response) => {
       if (!response.ok) {
-        alert('Wheather is not found');
+        alert('Current wheather is not found');
       }
       return response.json();
     })
-    .then((data) => showWeather(data));
+    .then((data) => displayCurrWeather(data));
   }
+  fetchCurrWeather('Poznan');
 
-  //* ========== displaying weather ==========
-  function showWeather(data) {
-    const {name} = data,
+  //* ========== displaying CURRENT weather ==========
+  function displayCurrWeather(data) {
+
+    //! console.log console.log console.log console.log console.log console.log console.log console.log
+    console.log(`CURRENT weather obj:`);
+    console.log(JSON.parse(JSON.stringify(data)));
+
+    const {name, visibility, id} = data,
       {speed} = data.wind,
-      {temp, humidity} = data.main,
-      {icon, description} = data.weather[0];
+      {temp, humidity, pressure} = data.main,
+      {icon, description} = data.weather[0],
+      {sunrise, sunset} = data.sys;
+
+    //* ========== converting sunset & sunrise ms to time ==========
+    function msToHours(ms) {
+      let hours = Math.floor( (ms / (1000 * 60 * 60) %  24) ),
+        minutes = Math.floor( (ms / 1000 / 60) % 60),
+
+      formattedHours = (hours < 10) ? "0" + hours : hours;
+      formattedMinutes = (minutes < 10) ? "0" + minutes : minutes;
+
+      return `${formattedHours}:${formattedMinutes}`;
+    }
+
+    currWeatherIco.src = `https://openweathermap.org/img/wn/${icon}.png`;
+    currWeahterTemp.innerText = `${temp} °C`;
+    currWeahterDescr.innerText = description.charAt(0).toUpperCase() + description.slice(1);
+    currWeatherCity.innerText = name;
+    currWeatherDate.innerText = new Date().toString().split(' ').splice(1,3).join(' ');
+    currWeatherSunrise.innerText = msToHours(sunrise);
+    currWeatherSunset.innerText = msToHours(sunset);
+    currWeatherHumidity.innerText = `${humidity} %`;
+    currWeatherWind.innerText = `${speed} km/h`,
+    currWeatherVisibility.innerText = `${Math.floor(visibility / 1000)} km`,
+    currWeatherPressure.innerText = pressure;
     
-  currWeatherIco.src = `https://openweathermap.org/img/wn/${icon}.png`;
-  currWeahterTemp.innerText = `${temp} °C`;
-  currWeahterDescr.innerText = description;
-  currWeatherCity.innerText = name;
-  currWeatherHumidity.innerText = `${humidity} %`;
-  currWeatherWind.innerText = `${speed} km/h`;
+    fetchTodayWeather(id);
   }
 
-  //* ===== checking for correct city before fetching weather =====
-  function search() {
-    correctValue = searchBar.value.split(' ').join('');
-    searchWeather(correctValue);
+  function fetchTodayWeather(id) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=${WEATHER_APIKEY}`) //? https://api.openweathermap.org/data/2.5/forecast?id=3088171&appid=e732a92c66574d856b927e390c09674e
+    .then((response) => {
+      if (!response.ok) {
+        alert('Today`s wheather is not found');
+      }
+      return response.json();
+    })
+    .then((data) => displayTodayWeather(data));
   }
+  
+  function displayTodayWeather(data) {
+    
+    //! console.log console.log console.log console.log console.log console.log console.log console.log
+    console.log(`TODAY weather obj:`);
+    console.log(JSON.parse(JSON.stringify(data)));
+  }
+
 
   //* ========== seaching wheather btns keyups ==========
   searchBar.addEventListener('keyup', function(event) {
@@ -78,7 +127,8 @@ window.addEventListener('DOMContentLoaded', function() {
   })
   searchBtn.addEventListener('click', function() {search()});
   
-  //! ========== DARK MODE THEME SWITCHER ==========
+
+  //! ============= DARK MODE THEME SWITCHER =============
   const themeSwitcher = document.querySelector('#themeSwitcher'),
     icoThemeSwitcher = themeSwitcher.querySelector('img'),
     spanThemeSwitcher = themeSwitcher.querySelector('span'),
